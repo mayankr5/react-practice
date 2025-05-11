124b5a02a1220feafb3228d772ed0b759a943313
Mayank <mayank525.work@gmail.com>
2025-05-11 19:47:21 +0530

    star-rating

--- src/components/star-rating/index.tsx
+++ src/components/star-rating/index.tsx
@@ -1,50 +1,50 @@
 import React, { useState } from 'react';
-import { FaStar } from 'react-icons/fa'
-import './styles.css'
+import { FaStar } from 'react-icons/fa';
+import './styles.css';
 
-const StarRating = () => {
-
+const StarRating: React.FC = () => {
     const [rating, setRating] = useState<number>(0);
-    const [hover, setHover] = useState<number>(0); 
+    const [hover, setHover] = useState<number>(0);
 
-    const handleClick = (index: number) => {
+    const handleClick = (index: number): void => {
         setRating(index);
-    }
+    };
 
-    const handleMouseEnter = (index: number) => {
+    const handleMouseEnter = (index: number): void => {
         setHover(index);
-    }
+    };
 
-    const handleMouseLeave = () => {
+    const handleMouseLeave = (): void => {
         setHover(rating);
-    }
+    };
 
-    const handleReset = () => {
+    const handleReset = (): void => {
         setHover(0);
         setRating(0);
-    }
+    };
 
     return (
         <div className="container">
-            <div className='star-container'>
-                { 
-                    [...Array(5)].map((_, index) => {
-                        index += 1;
-
-                        return <FaStar 
-                            className={ (index <= rating) || (index <= hover) ? 'active' : 'inactive'}
-                            key={index} 
-                            onClick={() => handleClick(index)}  
-                            onMouseEnter={() => handleMouseEnter(index)} 
+            <div className="star-container">
+                {[...Array(5)].map((_, i) => {
+                    const index = i + 1;
+                    return (
+                        <FaStar
+                            key={index}
+                            className={
+                                index <= (hover || rating) ? 'active' : 'inactive'
+                            }
+                            onClick={() => handleClick(index)}
+                            onMouseEnter={() => handleMouseEnter(index)}
                             onMouseLeave={handleMouseLeave}
                             size={40}
-                            />
-                    })
-                }
+                        />
+                    );
+                })}
             </div>
             <button onClick={handleReset}>Reset</button>
         </div>
-    )
-}
+    );
+};
 
 export default StarRating;
